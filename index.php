52a0fe0ef09875576a09022007459e627bda1683
Albin Varghese <albin8285@gmail.com>
2020-11-01 23:57:54 +0530

    add setDir()

--- index.php
+++ index.php
@@ -4,12 +4,21 @@
 class Renamer {
 	
 	private $prefix;
+	private $dir;
 	
-	function __construct($prefix) {
+	function __construct($prefix, $dir) {
 		
 		$this->prefix = $prefix;
+		$this->dir = $dir;
 	
 	}
+	
+	public function setDir($this->dir) {
+		if (isset($this->dir)) {
+		return opendir($this->dir);
+		} else { 
+			return "directory is empty";
+		}
 	
 }
 
