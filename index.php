56aa5f39c78d009c86958dd86fe9360d6a0ebab3
Albin Varghese <albin8285@gmail.com>
2020-11-02 01:06:08 +0530

    rename all files with given pattern

--- index.php
+++ index.php
@@ -22,8 +22,8 @@ while (false !== ($file = readdir($this->dir)))
 {
   $extension = strtolower(pathinfo($file, PATHINFO_EXTENSION));
     if (!empty($extension)) {
-        $newName = $this->prefix .'_' .$i . '_' . $extension . '.' . $extension. '<br>';
-        echo $newName;
+        $newName = $this->prefix .'_' .$i . '_' . $extension . '.' . $extension;
+        rename('upload/' . $file, 'upload/' . $newName);
         $i++;
     }
 }
