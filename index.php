dacb6900708a51e82ecef3a9963d156da6264c8d
Albin Varghese <albin8285@gmail.com>
2020-11-02 01:35:37 +0530

    validate arguements for setDir

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ class Renamer {
 	
 	private $prefix;
 	protected $dir;
+	protected $folder;
 	
 	function __construct($prefix) {
 		
@@ -13,28 +14,36 @@ class Renamer {
 	}
 		
 	public function setDir($dir) {
+		if (!empty($dir)) {
+		if(is_dir($dir)) { 
+		$this->folder = $dir;
 		$this->dir = opendir($dir);
+		} else {
+			echo "Invalid Directory";
+		exit();
+		}
+		} else {
+		echo "please specify a Directory";
+		exit();
+		}
 		}
 		
 	public function renamer() {
 		
-		
-		$i = 1;
-
+$i = 1;
 while (false !== ($file = readdir($this->dir)))
 {
   $extension = strtolower(pathinfo($file, PATHINFO_EXTENSION));
   $filename = strtolower(pathinfo($file, PATHINFO_FILENAME));
     if (!empty($extension)) {
         $newName = $this->prefix .'_' . $filename . '_' . $extension . '.' . $extension;
-        rename('upload/' . $file, 'upload/' . $newName);
+        rename($this->folder . $file, $this->folder . $newName);
         $i++;
     }
 }
 closedir($this->dir);
 		
 		}
-		
 		
 }
 
